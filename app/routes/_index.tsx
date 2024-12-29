0e6148608a0b829fb19cb7649e4d0b42afa95639
Peter Svehla <psvehla@hotmail.com>
2024-12-29 16:31:39 +1100

    savegame

---
+++ app/routes/_index.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <p id="index-page">
+      This is a Remix demo.
+      <br />
+      Check out{" "}
+      <a href="https://remix.run">the docs at remix.run</a>
+    </p>
+  );
+}
